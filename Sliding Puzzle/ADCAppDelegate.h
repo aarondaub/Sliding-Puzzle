3e3a5ebf5d20ef2c252b49e4a5d653fbfff0a468
Aaron <Aaron@Aarons-MacBook-Pro.local>
2014-02-05 21:20:27 -0800

    Initial Commit

---
+++ Sliding Puzzle/ADCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ADCAppDelegate.h
+//  Sliding Puzzle
+//
+//  Created by Aaron on 2/5/2014.
+//  Copyright (c) 2014 Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sliding Puzzle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sliding Puzzle
+//
+//  Created by Aaron on 2/5/2014.
+//  Copyright (c) 2014 Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ADCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ADCAppDelegate class]));
+    }
+}
